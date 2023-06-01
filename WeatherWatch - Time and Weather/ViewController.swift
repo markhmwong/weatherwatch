63f524bb549fcca9eb6038a55272a8ea0778b216
markhmwong <markhmwong@gmail.com>
2023-06-01 17:39:20 +0900

    Initial Commit

---
+++ WeatherWatch - Time and Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherWatch - Time and Weather
+//
+//  Created by Mark Wong on 1/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
